29d0bd22c15cced0cde710ccfde2231dd4de90e8
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2020-01-22 11:59:55 -0600

    Updates

---
+++ precache-manifest.94f90b383886c1730f6076bb6a75552c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da12cbf9047ecea6c189e2b5f87c3f0b",
+    "url": "/patients-react-practices/index.html"
+  },
+  {
+    "revision": "b90ebaab80beb8ec3a93",
+    "url": "/patients-react-practices/static/css/main.4c798faf.chunk.css"
+  },
+  {
+    "revision": "32540ebd4d4f5a8808e7",
+    "url": "/patients-react-practices/static/js/2.d79c13f7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/patients-react-practices/static/js/2.d79c13f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b90ebaab80beb8ec3a93",
+    "url": "/patients-react-practices/static/js/main.528b4959.chunk.js"
+  },
+  {
+    "revision": "2674b6647d464543ba21",
+    "url": "/patients-react-practices/static/js/runtime-main.b4e93d9e.js"
+  }
+]);
