efaa679fcca5fab65630426d93c629f050a38b3b
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2020-01-22 22:24:44 -0600

    implementing localStorage in the project, now, the appointment is save

--- src/component/Patients.js
+++ src/component/Patients.js
@@ -1,10 +1,27 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import Form from './Form';
 import Appointment from './Appointment'
 
 const Patients = () => {
+  // Citas en localstorage
+  // parse analiza un string como json
+  let initialAppointments = JSON.parse(localStorage.getItem('citas'))
+  if(!initialAppointments){
+    initialAppointments = []
+  }
+
   // Agregando un estado para citas, esas citas se van a almacenar aquí, en el componente principal
-  const [ allAppointment, setAllAppointment ] = useState([])
+  const [ allAppointment, setAllAppointment ] = useState(initialAppointments)
+ 
+  // useEffect parra realizar ciertas operaciones cuándo el state cambia
+  useEffect( () => {
+    if(initialAppointments){
+      //Stringify lo está convirtiendo a un arreglo porque es lo único que soporta localstorage
+      localStorage.setItem('citas', JSON.stringify(allAppointment))
+    }else{
+      localStorage.setItem('citas', JSON.stringify([]))
+    }
+  }, [allAppointment]);      
 
   // Función que tome las citas actuales y agregué la nueva
   const createAppointment = (appoint) => { 
